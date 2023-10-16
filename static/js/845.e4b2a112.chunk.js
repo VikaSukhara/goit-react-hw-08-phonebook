c6169201d6a4878e4fb6dbe7e7849ebd07f05a18
VikaSukhara <125195456+VikaSukhara@users.noreply.github.com>
2023-10-16 20:38:34 +0000

    Deploying to gh-pages from @ VikaSukhara/goit-react-hw-08-phonebook@d7f12e67ca84db6400cfe66f9e75109a5cba8979 🚀

---
+++ static/js/845.e4b2a112.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[845],{9845:function(e,o,t){t.r(o);var r=t(184);o.default=function(){return(0,r.jsxs)("div",{children:[(0,r.jsx)("h1",{children:"Welcome to the phone book"}),(0,r.jsx)("p",{children:"Register or Log in to use the phone book"})]})}}}]);
+//# sourceMappingURL=845.e4b2a112.chunk.js.map
